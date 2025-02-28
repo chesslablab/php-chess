57e488b8d5898d5860e3cec5a935c49bbb7720fe
standard <info@programarivm.com>
2025-02-28 17:16:24 +0100

    Updated BoardToMp4Test

--- tests/unit/Media/BoardToMp4Test.php
+++ tests/unit/Media/BoardToMp4Test.php
@@ -4,9 +4,8 @@ namespace Chess\Tests\Unit\Media;
 
 use Chess\Media\BoardToMp4;
 use Chess\Tests\AbstractUnitTestCase;
-use Chess\Variant\Chess960\Board as Chess960Board;
-use Chess\Variant\Classical\Board as ClassicalBoard;
-use Chess\Variant\Classical\FenToBoardFactory;
+use Chess\Variant\Chess960\FenToBoardFactory as Chess960FenToBoardFactory;
+use Chess\Variant\Classical\FenToBoardFactory as ClassicalFenToBoardFactory;
 
 class BoardToMp4Test extends AbstractUnitTestCase
 {
@@ -24,7 +23,7 @@ class BoardToMp4Test extends AbstractUnitTestCase
     {
         $A74 = file_get_contents(self::DATA_FOLDER.'/opening/A74.pgn');
 
-        $board = new ClassicalBoard();
+        $board = ClassicalFenToBoardFactory::create();
 
         $filename = (new BoardToMp4(
             $A74,
@@ -40,11 +39,7 @@ class BoardToMp4Test extends AbstractUnitTestCase
      */
     public function output_960_QRKRNNBB()
     {
-        $fen = 'qrkr1nbb/pppp2pp/3n1p2/4p3/4P3/4NP2/PPPP2PP/QRKRN1BB w KQkq -';
-
-        $shuffle = ['Q', 'R', 'K', 'R', 'N', 'N', 'B', 'B'];
-
-        $board = FenToBoardFactory::create($fen, new Chess960Board($shuffle));
+        $board = Chess960FenToBoardFactory::create('qrkr1nbb/pppp2pp/3n1p2/4p3/4P3/4NP2/PPPP2PP/QRKRN1BB w KQkq -');
 
         $movetext = '1.Bf2 Re8 2.Nd3 O-O-O 3.O-O';
 
@@ -62,11 +57,7 @@ class BoardToMp4Test extends AbstractUnitTestCase
      */
     public function output_960_BNNBQRKR()
     {
-        $fen = 'b4rkr/ppppqppp/2nnpb2/8/4P3/2PP4/PP1NNPPP/B2BQRKR w KQkq -';
-
-        $shuffle = ['B', 'N', 'N', 'B', 'Q', 'R', 'K', 'R'];
-
-        $board = FenToBoardFactory::create($fen, new Chess960Board($shuffle));
+        $board = Chess960FenToBoardFactory::create('b4rkr/ppppqppp/2nnpb2/8/4P3/2PP4/PP1NNPPP/B2BQRKR w KQkq -');
 
         $movetext = '1.Bc2 O-O-O 2.Qc1 Rhe8 3.Rd1 h6 4.O-O';
 
