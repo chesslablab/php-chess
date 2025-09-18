ee02394996e606db5cf1913c5e88fca19b6085c7
rfc1123 <info@chesslablab.org>
2025-09-18 16:43:18 +0200

    Refactored Chess\Variant\PieceArrayFactory

--- src/Variant/PieceArrayFactory.php
+++ src/Variant/PieceArrayFactory.php
@@ -4,10 +4,8 @@ namespace Chess\Variant;
 
 use Chess\Variant\AbstractNotation;
 use Chess\Variant\Classical\R;
-use Chess\Variant\Classical\PGN\Castle;
 use Chess\Variant\Classical\PGN\Color;
 use Chess\Variant\Classical\PGN\Piece;
-use Chess\Variant\Classical\PGN\Square;
 use Chess\Variant\Classical\CastlingRule;
 
 class PieceArrayFactory
@@ -31,13 +29,7 @@ class PieceArrayFactory
                         $color = Color::W;
                     }
                     if ($char === Piece::R) {
-                        if ($sq === $castlingRule?->rule[$color][Piece::R][Castle::LONG]['from']) {
-                            $pieces[] = new R($color, $sq, $square, RType::CASTLE_LONG);
-                        } elseif ($sq === $castlingRule?->rule[$color][Piece::R][Castle::SHORT]['from']) {
-                            $pieces[] = new R($color, $sq, $square, RType::CASTLE_SHORT);
-                        } else {
-                            $pieces[] = new R($color, $sq, $square, RType::R);
-                        }
+                        $pieces[] = new R($color, $sq, $square, RType::R);
                     } else {
                         $class = VariantType::getClass($char, $namespace);
                         $pieces[] = new $class($color, $sq, $square);
